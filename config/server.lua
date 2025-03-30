cdc52f99dc87acaaf98cffe4903f1d7038428919
Smithy <96345069+Smitthyy@users.noreply.github.com>
2025-03-30 23:28:10 +0100

    Prevent financed vehicle selling

--- config/server.lua
+++ config/server.lua
@@ -4,7 +4,7 @@ return {
         paymentWarning = 10, -- time in minutes that player has to make payment before repo
         paymentInterval = 24, -- time in hours between payment being due
         cronSchedule = '*/10 * * * *', -- cron schedule for finance payment checks ref: https://overextended.dev/ox_lib/Modules/Cron/Server#cron-expression
-        preventSelling = false, -- prevents players from using /transfervehicle if financed
+        preventSelling = true, -- prevents players from using /transfervehicle if financed
     },
     saleTimeout = 60000, -- Delay between attempts to sell/gift a vehicle. Prevents abuse
     deleteUnpaidFinancedVehicle = false, -- true to delete unpaid vehicles from database, otherwise it will edit citizenid to hide from db select
